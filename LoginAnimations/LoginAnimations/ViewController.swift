fbd7113880a7a2738146ee640966a8a508419282
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-09-20 13:45:13 +0300

    LoginAnimations Project Created

---
+++ LoginAnimations/LoginAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginAnimations
+//
+//  Created by Marian Stanciulica on 20/09/2018.
+//  Copyright © 2018 Marian Stanciulica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
