2a05ad32abe77c29de70b453af91affc5b82b6e0
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-09-24 09:30:51 +0300

    FlightAnimations - User Interface Configured

--- FlightAnimations/FlightAnimations/Controllers/ViewController.swift
+++ FlightAnimations/FlightAnimations/Controllers/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var summaryIcon: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var flightNumberLabel: UILabel!
+    @IBOutlet weak var gateNumberLabel: UILabel!
+    @IBOutlet weak var departingFromLabel: UILabel!
+    @IBOutlet weak var arrivingToLabel: UILabel!
+    @IBOutlet weak var planeImageView: UIImageView!
+    @IBOutlet weak var flightStatusLabel: UILabel!
+    @IBOutlet weak var statusImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
