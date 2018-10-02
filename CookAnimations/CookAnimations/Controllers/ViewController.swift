1a5fccbc423cb208f3bce0cdf739d8402c043a58
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-02 14:54:51 +0300

    CookAnimations - Project Created

---
+++ CookAnimations/CookAnimations/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CookAnimations
+//
+//  Created by Marian Stanciulica on 02/10/2018.
+//  Copyright © 2018 Marian Stanciulica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
