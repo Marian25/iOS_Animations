c9980a1a2f728ea97c8b5ebd119f7cc69d843946
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-09 16:53:58 +0300

    SnowAnimation - User Interface Configured

--- SnowAnimation/SnowAnimation/Controllers/ViewController.swift
+++ SnowAnimation/SnowAnimation/Controllers/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
+// MARK: - Delay Function
+
+func delay(seconds: Double, completion: @escaping () -> ()) {
+    let milliseconds = Int(1000 * seconds)
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(milliseconds)) {
+        completion()
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var summaryIcon: UIImageView!
+    @IBOutlet weak var summary: UILabel!
+    @IBOutlet weak var flightNumber: UILabel!
+    @IBOutlet weak var departingFrom: UILabel!
+    @IBOutlet weak var arrivingTo: UILabel!
+    @IBOutlet weak var gateNumber: UILabel!
+    @IBOutlet weak var planeImage: UIImageView!
+    @IBOutlet weak var statusBanner: UIImageView!
+    @IBOutlet weak var flightStatus: UILabel!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        summary.addSubview(summaryIcon)
+        summaryIcon.center.y = summary.frame.size.height / 2
+        
+        changeFlightDataTo(data: londonToParis)
     }
 
-
+    func changeFlightDataTo(data: FlightData) {
+        summary.text = data.summary
+        flightNumber.text = data.flightNr
+        gateNumber.text = data.gateNr
+        departingFrom.text = data.departingFrom
+        arrivingTo.text = data.arrivingTo
+        flightStatus.text = data.flightStatus
+        backgroundImageView.image = UIImage(named: data.weatherImageName)
+    }
+    
 }
 
