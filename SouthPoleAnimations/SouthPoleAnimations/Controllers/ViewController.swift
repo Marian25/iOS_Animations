e1a4df94cea3da21aa6e86453d7c6b22e690b61b
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-09 17:53:56 +0300

    SouthPoleAnimations - User Interface Configured

--- SouthPoleAnimations/SouthPoleAnimations/Controllers/ViewController.swift
+++ SouthPoleAnimations/SouthPoleAnimations/Controllers/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var penguin: UIImageView!
+    @IBOutlet weak var slideButton: UIButton!
+
+    var isLookingRight = true
+    var penguinY: CGFloat!
+    
+    var walkSize: CGSize!
+    var slideSize: CGSize!
+    
+    let animationDuration = 1.0
+    
+    var walkFrames = [
+        UIImage(named: "walk01.png")!,
+        UIImage(named: "walk02.png")!,
+        UIImage(named: "walk03.png")!,
+        UIImage(named: "walk04.png")!
+    ]
+    
+    var slideFrames = [
+        UIImage(named: "slide01.png")!,
+        UIImage(named: "slide02.png")!,
+        UIImage(named: "slide01.png")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        walkSize = walkFrames[0].size
+        slideSize = slideFrames[0].size
+        penguinY = penguin.frame.origin.y
+    }
+    
+    func loadWalkAnimation() {
+        
+    }
+    
+    func loadSlideAnimation() {
+        
     }
 
-
+    @IBAction func actionLeft(_ sender: Any) {
+        
+    }
+    
+    @IBAction func actionRight(_ sender: Any) {
+    
+    }
+    
+    @IBAction func actionSlide(_ sender: Any) {
+    
+    }
 }
 
