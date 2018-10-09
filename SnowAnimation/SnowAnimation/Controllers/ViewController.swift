c068b8f9a29d025dd5c2b8972e28b5ea8c348b32
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-09 17:27:18 +0300

    SnowAnimation - Particle Emitters

--- SnowAnimation/SnowAnimation/Controllers/ViewController.swift
+++ SnowAnimation/SnowAnimation/Controllers/ViewController.swift
@@ -20,6 +20,8 @@ func delay(seconds: Double, completion: @escaping () -> ()) {
 
 class ViewController: UIViewController {
 
+    let flakes = ["flake", "flake1", "flake2", "flake3", "flake4"]
+    
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var summaryIcon: UIImageView!
     @IBOutlet weak var summary: UILabel!
@@ -42,6 +44,48 @@ class ViewController: UIViewController {
         summaryIcon.center.y = summary.frame.size.height / 2
         
         changeFlightDataTo(data: londonToParis)
+        
+        let rect = CGRect(x: 0.0, y: -70.0, width: view.bounds.width, height: 50.0)
+        let emitter = CAEmitterLayer()
+        emitter.frame = rect
+        view.layer.addSublayer(emitter)
+        
+        emitter.emitterShape = CAEmitterLayerEmitterShape.rectangle
+        emitter.emitterPosition = CGPoint(x: rect.width / 2, y: rect.height / 2)
+        emitter.emitterSize = rect.size
+        
+        for flake in flakes {
+            let emitterCell = makeEmitterCell(with: flake)
+            if emitter.emitterCells == nil {
+                emitter.emitterCells = [emitterCell]
+            } else {
+                emitter.emitterCells!.append(emitterCell)
+            }
+        }
+    }
+    
+    func makeEmitterCell(with imageNamed: String) -> CAEmitterCell {
+        let emitterCell = CAEmitterCell()
+        emitterCell.contents = UIImage(named: imageNamed)?.cgImage
+        emitterCell.birthRate = 50
+        emitterCell.lifetime = 3.5
+        emitterCell.yAcceleration = 70.0
+        emitterCell.xAcceleration = 10.0
+        emitterCell.velocity = 20.0
+        emitterCell.emissionLongitude = -.pi
+        emitterCell.velocityRange = 200.0
+        emitterCell.emissionRange = .pi * 0.5
+        emitterCell.color = UIColor(red: 0.9, green: 1.0, blue: 1.0, alpha: 1.0).cgColor
+        emitterCell.redRange = 0.1
+        emitterCell.greenRange = 0.1
+        emitterCell.blueRange = 0.1
+        emitterCell.scale = 0.8
+        emitterCell.scaleRange = 0.8
+        emitterCell.scaleSpeed = -0.15
+        emitterCell.alphaRange = 0.75
+        emitterCell.alphaSpeed = -0.15
+        emitterCell.lifetimeRange = 1.0
+        return emitterCell
     }
 
     func changeFlightDataTo(data: FlightData) {
