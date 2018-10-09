c7c35284dd8b90ab77ce86585072f8385b0acc48
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-09 18:20:32 +0300

    SouthPoleAnimations - Frame Animations with UIImageView

--- SouthPoleAnimations/SouthPoleAnimations/Controllers/ViewController.swift
+++ SouthPoleAnimations/SouthPoleAnimations/Controllers/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var penguin: UIImageView!
     @IBOutlet weak var slideButton: UIButton!
 
-    var isLookingRight = true
+    var isLookingRight: Bool = true {
+        didSet {
+            let xScale: CGFloat = isLookingRight ? 1 : -1
+            penguin.transform = CGAffineTransform(scaleX: xScale, y: 1)
+            slideButton.transform = penguin.transform
+        }
+    }
+    
     var penguinY: CGFloat!
     
     var walkSize: CGSize!
@@ -40,26 +47,58 @@ class ViewController: UIViewController {
         walkSize = walkFrames[0].size
         slideSize = slideFrames[0].size
         penguinY = penguin.frame.origin.y
+
+        loadWalkAnimation()
     }
     
     func loadWalkAnimation() {
-        
+        penguin.animationImages = walkFrames
+        penguin.animationDuration = animationDuration / 3
+        penguin.animationRepeatCount = 3
     }
     
     func loadSlideAnimation() {
-        
+        penguin.animationImages = slideFrames
+        penguin.animationDuration = animationDuration
+        penguin.animationRepeatCount = 1
     }
 
     @IBAction func actionLeft(_ sender: Any) {
+        isLookingRight = false
+        penguin.startAnimating()
         
+        UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseOut, animations: {
+            self.penguin.center.x -= self.walkSize.width
+        }, completion: nil)
     }
     
     @IBAction func actionRight(_ sender: Any) {
-    
+        isLookingRight = true
+        penguin.startAnimating()
+        
+        UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseOut, animations: {
+            self.penguin.center.x += self.walkSize.width
+        }, completion: nil)
     }
     
     @IBAction func actionSlide(_ sender: Any) {
-    
+        loadSlideAnimation()
+        
+        penguin.frame = CGRect(x: penguin.frame.origin.x,
+                               y: penguinY + walkSize.height - slideSize.height,
+                               width: slideSize.width,
+                               height: slideSize.height)
+        penguin.startAnimating()
+        
+        UIView.animate(withDuration: animationDuration - 0.02, delay: 0.0, options: .curveEaseOut, animations: {
+            self.penguin.center.x += self.isLookingRight ? self.slideSize.width : -self.slideSize.width
+        }) { _ in
+            self.penguin.frame = CGRect(x: self.penguin.frame.origin.x,
+                                        y: self.penguinY,
+                                        width: self.walkSize.width,
+                                        height: self.walkSize.height)
+            self.loadWalkAnimation()
+        }
     }
 }
 
